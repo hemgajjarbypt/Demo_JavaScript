8414d89b05a7dfcabcfef045506ff8009c1e99cd
hemgajjarbypt <hem.gajjar@bypeopletechnologies.com>
2024-01-30 11:24:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -294,6 +294,5 @@ const movies = [
     {title:'d', year: 2017, rating: 4.5}
 ]
 
-const titles = movies.filter(m => m.year === 2018 && m.rating >= 4).sort((a,b) => a.rating - b.rating).reverse().map(m => m.title);
-
-console.log(titles);
+// const titles = movies.filter(m => m.year === 2018 && m.rating >= 4).sort((a,b) => a.rating - b.rating).reverse().map(m => m.title);
+// console.log(titles);
